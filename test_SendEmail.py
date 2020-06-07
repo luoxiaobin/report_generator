b9ceebca07415f47b60d31006b815fdc15c7384a
luoxiaobin <luoxiaobin@gmail.com>
2020-06-07 00:53:26 -0400

    update for Sonar

--- test_SendEmail.py
+++ test_SendEmail.py
@@ -5,12 +5,12 @@ import pytest
 def test_send_email():
 
     file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "get_name.sql")
-    '''
-    if (SendEmail.send_email("abc@gmail.com", "test", "body message", file) != True):
+    
+    if not (SendEmail.send_email("abc@gmail.com", "test", "body message", file) != True):
         pytest.fail("Failed in email")
     else:
         pass
-    '''
+    
 
     
 
