ae9b074f7ccf0f3a0d8af8f0521f6710c73321eb
luoxiaobin <luoxiaobin@gmail.com>
2020-06-07 01:03:46 -0400

    fix test_sendemail

--- test_SendEmail.py
+++ test_SendEmail.py
@@ -6,10 +6,8 @@ def test_send_email():
 
     file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "get_name.sql")
     
-    if not (SendEmail.send_email("abc@gmail.com", "test", "body message", file) != True):
-        pytest.fail("Failed in email")
-    else:
-        pass
+    if (SendEmail.send_email("abc@gmail.com", "test", "body message", file) != True):
+       pass
     
 
     
