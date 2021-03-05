7e70b3ddb808b57371ec166e1cbba70035fb8656
Kevin Luo <5904290+luoxiaobin@users.noreply.github.com>
2021-03-04 21:11:24 -0500

    Update DBInquiry.py
    Minor change

--- DBInquiry.py
+++ DBInquiry.py
@@ -19,7 +19,7 @@ def run_sql_string(sql_string):
     config = configparser.ConfigParser()
     config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'database.ini'))
 
-    db_type = config.get('General', 'db_type')
+    db_type = config.get('General', 'db_type') 
 
     conn = None
     if (db_type == 'SQLite'):
